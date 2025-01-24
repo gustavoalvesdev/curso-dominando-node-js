60f44ae521c004e2b10381af7b1d42a7861077d4
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-01-24 19:24:28 -0300

    Consumindo os parâmetros de Query e Route

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const express = require('express');
 const server = express();
 
 server.get('/hello', (req, res) => {
+    const nome = req.query.nome;
+    const idade = req.query.idade;
     res.json({
         title: 'Hello World',
-        message: "Olá meu amigo tudo bem!?"
+        message: `Olá ${nome} tudo bem!?`,
+        idade
+    });
+});
+
+server.get('/hello/:nome', (req, res) => {
+    const { nome } = req.params;
+    res.json({
+        title: 'Hello World',
+        message: `Olá ${nome} tudo bem!?`
     });
 });
 
